8beab7ed3d206c2c9d9bba23e3c75f89c0cae316
zhangjin <798101251@qq.com>
2020-12-16 20:56:16 +0800

    1.login

---
+++ login/src/main/java/com/zj/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.zj.login
+
+import com.zj.common.base.BaseActivity
+import com.zj.login.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>() {
+
+    override fun getLayoutId() = R.layout.activity_login
+}
